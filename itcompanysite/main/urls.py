9f78dccaf2881b144a49d3c1cb64204ef3deaa5c
Boris <BacR009@yandex.ru>
2023-11-08 11:00:23 +0500

    image on site now

--- itcompanysite/main/urls.py
+++ itcompanysite/main/urls.py
@@ -1,8 +1,13 @@
 from . import views
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('', views.index, name='home'),
     path('companies/', include('companies.url')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
