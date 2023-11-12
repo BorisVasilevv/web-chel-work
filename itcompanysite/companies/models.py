3215b7ab2da9a23602751194c6f8ac7e893a6663
Alexandra Leonidova <alexandraleonid2002@gmail.com>
2023-11-12 23:43:16 +0500

    companies change 2

--- itcompanysite/companies/models.py
+++ itcompanysite/companies/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+
 class Company(models.Model):
     name = models.CharField('name', max_length=150)
     logotype = models.ImageField('logotype', upload_to='main/static/main/img')
     short_description = models.TextField('short_description')
+    url = models.CharField('url', max_length=500)
 
     class Meta:
         verbose_name = 'Company'
--- itcompanysite/companies/views.py
+++ itcompanysite/companies/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def napoleonit(request):
     return render(request, 'companies/napoleonit.html')
+
 
 def companies(request):
     return render(request, 'companies/companies.html')
