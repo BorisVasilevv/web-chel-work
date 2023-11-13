8b15cb8d68972ec1d4d419b9ba622f14e6fc8bbd
Boris <BacR009@yandex.ru>
2023-11-13 18:00:36 +0500

    start search func

--- itcompanysite/companies/urls.py
+++ itcompanysite/companies/urls.py
@@ -2,8 +2,9 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('napoleonit/', views.napoleonit, name='napoleonit'),
-    path('', views.companies, name='companies')
+
+    path('', views.companies, name='companies'),
+    path('search/', views.search, name='search')
 ]
 
 
--- itcompanysite/companies/views.py
+++ itcompanysite/companies/views.py
@@ -3,11 +3,12 @@ from .models import Company
 # Create your views here.
 
 
-def napoleonit(request):
-    return render(request, 'companies/napoleonit.html')
-
-
 def companies(request):
+    companies = Company.objects.all()
+    return render(request, 'companies/companies.html', {'companies': companies})
+
+
+def search(request):
     companies = Company.objects.all()
     return render(request, 'companies/companies.html', {'companies': companies})
 
