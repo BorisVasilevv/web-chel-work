6f7ee9e987f0636492f650240a2206cf9615a296
Boris <BacR009@yandex.ru>
2023-11-14 23:01:06 +0500

    fix bugs after merge main page

--- itcompanysite/companies/urls.py
+++ itcompanysite/companies/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 urlpatterns = [
 
     path('', views.companies, name='companies'),
-    path('search/', views.search, name='search')
+    path('search/', views.search, name='search'),
+    path('<str:category_name>/', views.companies_per_type, name='companies_per_type')
 ]
 
 
