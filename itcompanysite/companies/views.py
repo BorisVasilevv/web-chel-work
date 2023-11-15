8057e7c20b20d9545098a9bee2fb17b06ce1874b
Boris <BacR009@yandex.ru>
2023-11-15 17:10:51 +0500

    start relation of db and company type

--- itcompanysite/companies/views.py
+++ itcompanysite/companies/views.py
@@ -7,12 +7,13 @@ def companies(request):
     companies = Company.objects.all()
     return render(request, 'companies/companies.html', {'companies': companies})
 
+
 def companies_per_type(request, category_name):
-    if(category_name == "CustomDev"):
+    if category_name == "CustomDev":
         type_id = 1
-    if (category_name == "SelfProducts"):
+    if category_name == "SelfProducts":
         type_id = 2
-    if (category_name == "B2G"):
+    if category_name == "B2G":
         type_id = 3
     companies = Company.objects.filter(type_id = type_id)
     return render(request, 'companies/companies.html', {'companies': companies})
