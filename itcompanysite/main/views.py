0ab7fb071c1a2d2aa4a4ebec2fcb5b3d69d016c0
Boris <BacR009@yandex.ru>
2023-11-15 23:57:56 +0500

    test

--- itcompanysite/main/views.py
+++ itcompanysite/main/views.py
@@ -6,7 +6,7 @@ from companies.models import CompanyType
 # Create your views here.
 def index(request):
     company_types = CompanyType.objects.all()
-    return render(request, 'main/index.html', {"companyTypes": company_types})
+    return render(request, 'main/index.html', {"company_types": company_types})
 
 
 def map(request):
