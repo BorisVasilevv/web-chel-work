c0470054485b94ff1aa02fb5022a249f7adf9333
Boris <BacR009@yandex.ru>
2023-11-15 18:56:29 +0500

    code review with company type

--- itcompanysite/companies/views.py
+++ itcompanysite/companies/views.py
@@ -8,14 +8,15 @@ def companies(request):
     return render(request, 'companies/companies.html', {'companies': companies})
 
 def companies_per_type(request, category_name):
-    if(category_name == "CustomDev"):
-        type_id = 1
-    if (category_name == "SelfProducts"):
-        type_id = 2
-    if (category_name == "B2G"):
-        type_id = 3
-    companies = Company.objects.filter(type_id = type_id)
-    return render(request, 'companies/companies.html', {'companies': companies})
+    # if(category_name == "CustomDev"):
+    #     type_id = 2
+    # if (category_name == "SelfProducts"):
+    #     type_id = 4
+    # if (category_name == "B2G"):
+    #     type_id = 3
+    cp = CompanyType.objects.get(type_name__contains=category_name)
+    companies = Company.objects.filter(type_id=cp.id)
+    return render(request, 'companies/companies.html',  {'companies': companies})
 
 def search(request):
     companies = Company.objects.filter(name__contains=request.GET.get("q"))
