741acd67c0affdf0a6769d7bd56cbf8c2c931c21
Alexandra Leonidova <alexandraleonid2002@gmail.com>
2023-11-15 16:42:23 +0500

    profile and logout joining

--- itcompanysite/accounts/urls.py
+++ itcompanysite/accounts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('registration/', views.registration, name='registration'),
+    path('profile/', views.profile, name='profile'),
     path('', include('django.contrib.auth.urls'))
+
 ]
