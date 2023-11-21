652d37f8b13996e982607451607d510989879384
Boris <BacR009@yandex.ru>
2023-11-21 23:07:34 +0500

    style change

---
+++ itcompanysite/main/static/main/js/scripts.js
@@ -0,0 +1,33 @@
+//var element1 = document.getElementById("categories-item_details__1");
+//element1.addEventListener("click", (event) => showDescription(event, "categories-item_details__1", "categories-item__1"));
+//
+//var element2 = document.getElementById("categories-item_details__2");
+//element2.addEventListener("click", (event) => showDescription(event, "categories-item_details__2", "categories-item__2"));
+//
+//var element3 = document.getElementById("categories-item_details__3");
+//element3.addEventListener("click", (event) => showDescription(event, "categories-item_details__3", "categories-item__3"));
+//
+//var element4 = document.getElementById("categories-item_details__4");
+//element4.addEventListener("click", (event) => showDescription(event, "categories-item_details__4", "categories-item__4"));
+//
+//var element5 = document.getElementById("categories-item_details__5");
+//element5.addEventListener("click", (event) => showDescription(event, "categories-item_details__5", "categories-item__5"));
+//
+//var element6 = document.getElementById("categories-item_details__6");
+//element6.addEventListener("click", (event) => showDescription(event, "categories-item_details__6", "categories-item__6"));
+//
+//var element7 = document.getElementById("categories-item_details__7");
+//element7.addEventListener("click", (event) => showDescription(event, "categories-item_details__7", "categories-item__7"));
+//
+//var element8 = document.getElementById("categories-item_details__8");
+//element8.addEventListener("click", (event) => showDescription(event, "categories-item_details__8", "categories-item__8"));
+//
+//function showDescription(event, categoriesItemDetailsId ,categoriesItemId) {
+//    let categoriesItemDetails = document.getElementById(categoriesItemDetailsId);
+//    let categoriesItem = document.getElementById(categoriesItemId);
+//    if (categoriesItemDetails.open){
+//    categoriesItem.style.width = "250px"; // скрыть div при открытии details
+//    } else {
+//    categoriesItem.style.width = "500px"; // показать div при закрытии details
+//    }
+//}
