822b1bb4c4eba877c514e613518d19c8252c6c7a
Boris <BacR009@yandex.ru>
2023-11-21 18:10:57 +0500

    add fixtures for company

--- itcompanysite/companies/views.py
+++ itcompanysite/companies/views.py
@@ -9,17 +9,18 @@ def companies(request):
 
 
 def companies_per_type(request, category_name):
-    # if(category_name == "CustomDev"):
-    #     type_id = 2
-    # if (category_name == "SelfProducts"):
-    #     type_id = 4
-    # if (category_name == "B2G"):
-    #     type_id = 3
     cp = CompanyType.objects.get(type_name__contains=category_name)
     companies = Company.objects.filter(type_id=cp.id)
     return render(request, 'companies/companies.html',  {'companies': companies})
 
 def search(request):
-    companies = Company.objects.filter(name__contains=request.GET.get("q"))
+    query = request.GET.get("q")
+    query = query.lower()
+    companies = Company.objects.filter(name__icontains=query)
+    if(companies.__len__()==0):
+        query = query.title()
+        companies = Company.objects.filter(name__icontains=query)
+
+
     return render(request, 'companies/companies.html', {'companies': companies})
 
