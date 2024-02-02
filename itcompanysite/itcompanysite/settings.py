3e7b256554463f1728e074eb82221fa9279a3d91
Boris <BacR009@yandex.ru>
2024-02-03 00:04:46 +0500

    change structure env

--- itcompanysite/itcompanysite/settings.py
+++ itcompanysite/itcompanysite/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-74^7e^&ke!8zn4njk$+q5865+^1kvi#7po*rbsqi3jmba4_=74'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
