029a968fa1a6caa996e496f58e1748b29d991b6c
Boris <BacR009@yandex.ru>
2024-02-11 20:20:04 +0500

    add tags

--- itcompanysite/companies/admin.py
+++ itcompanysite/companies/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Company, Category, Subcategory, CompanyCategory, Favorite, Address, City, CompanyAddress
+from .models import Company, Category, Subcategory, CompanyCategory, Favorite, \
+    Address, City, CompanyAddress, Tag, CompanyTag
 # Register your models here.
 
 
@@ -15,3 +16,5 @@ admin.site.register(CompanyCategory)
 admin.site.register(Address)
 admin.site.register(City)
 admin.site.register(CompanyAddress)
+admin.site.register(Tag)
+admin.site.register(CompanyTag)
