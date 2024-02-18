afb113bb6af9c4d40865ba306ecb727585d4db40
Nikita Ershov <137996541+ersnick@users.noreply.github.com>
2024-02-18 22:34:22 +0500

    now all data

--- itcompanysite/work-with-data/dump_data.py
+++ itcompanysite/work-with-data/dump_data.py
@@ -1,9 +1,17 @@
 import subprocess
+import os
+
+full_path = os.path.abspath(__file__)
+folder_name = os.path.basename(os.path.dirname(full_path))
+
+if folder_name == 'work-with-data':
+    dumpdata_path = r'dumpdata.ps1'
+else:
+    dumpdata_path = r'work-with-data\dumpdata.ps1'
 
 powershell_path = r'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
-ps1_file1 = r'dumpdata.ps1'
 
-result = subprocess.run([powershell_path, '-File', ps1_file1], capture_output=True, text=True)
+result = subprocess.run([powershell_path, '-File', dumpdata_path], capture_output=True, text=True)
 
 if result.returncode == 0:
     print("Команда выполнена успешно.")
--- itcompanysite/work-with-data/load_data.py
+++ itcompanysite/work-with-data/load_data.py
@@ -1,9 +1,17 @@
 import subprocess
+import os
+
+full_path = os.path.abspath(__file__)
+folder_name = os.path.basename(os.path.dirname(full_path))
+
+if folder_name == 'work-with-data':
+    loaddata_path = r'loaddata.ps1'
+else:
+    loaddata_path = r'work-with-data\loaddata.ps1'
 
 powershell_path = r'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
-ps1_file1 = r'loaddata.ps1'
 
-result = subprocess.run([powershell_path, '-File', ps1_file1], capture_output=True, text=True)
+result = subprocess.run([powershell_path, '-File', loaddata_path], capture_output=True, text=True)
 
 if result.returncode == 0:
     print("Команда выполнена успешно.")
