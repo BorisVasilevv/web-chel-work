b5c5715eac12f048c7508c4ca09c5ba2334f2efa
Nikita Ershov <137996541+ersnick@users.noreply.github.com>
2024-02-22 03:10:38 +0500

    geocoder in admin panel
    legacy code in future

--- itcompanysite/companies/utils.py
+++ itcompanysite/companies/utils.py
@@ -1,4 +1,10 @@
 import re
+import requests
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 
 def has_russian_letters(input_string):
@@ -7,3 +13,32 @@ def has_russian_letters(input_string):
 
     russian_letters_pattern = re.compile('[а-яА-Я]')
     return bool(russian_letters_pattern.search(input_string))
+
+
+def geocoder(address):
+    url = "https://api.geotree.ru/address.php"
+    parameters = {'key': env('GEO_KEY'), "term": address}
+
+    response = requests.get(url, params=parameters)
+
+    # проверяем успешность запроса
+    if response.status_code == 200:
+        # получаем JSON-ответ
+        json_response = response.json()
+
+        # доступ к координатам
+        try:
+            first_result = json_response[0]
+            geo_center = first_result['geo_center']
+            latitude = geo_center['lat']
+            longitude = geo_center['lon']
+            coordinates = [latitude, longitude]
+
+            return coordinates
+
+        except (IndexError, KeyError) as e:
+            print(f"Ошибка при извлечении координат: {e}")
+            return None
+    else:
+        print(f"Ошибка при выполнении запроса: {response.status_code}")
+        return None
